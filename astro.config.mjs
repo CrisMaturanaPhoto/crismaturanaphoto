8e724ca07d61fbf139e0f6d1f76332caa30cb722
CrisMaturanaPhoto <maturanagormaz@gmail.com>
2026-05-26 18:24:46 +0700

    feat: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://crismaturana.com',
   compressHTML: true,
+
   build: {
     assets: '_assets',
   },
+
+  integrations: [sitemap()],
 });
